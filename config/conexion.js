1eee1d1b5ac5525b5a63ede2e1df58df065334f0
DESKTOP-NJKD4HU\andry <laviejaescuelac@gmail.com>
2018-05-01 14:07:35 -0400

    subida posible solucion

--- config/conexion.js
+++ config/conexion.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'fdb17.runhosting.com',
-  user     : '2406907_recetas',
-  password : '12recetas',
-  database : '2406907_recetas'
+  host     : 'sql10.freemysqlhosting.net',
+  user     : 'sql10235766',
+  password : 'JXuHSX1dFJ',
+  database : 'sql10235766'
 });
 
 module.exports = connection
