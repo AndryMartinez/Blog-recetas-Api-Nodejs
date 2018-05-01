f47e7a4ad56d7d330a3d483aed1d6d6b9cab67c3
DESKTOP-NJKD4HU\andry <laviejaescuelac@gmail.com>
2018-05-01 14:30:59 -0400

    subida solucion 2

--- config/conexion.js
+++ config/conexion.js
@@ -1,6 +1,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'sql10.freemysqlhosting.net',
+  port     : 3306,
   user     : 'sql10235766',
   password : 'JXuHSX1dFJ',
   database : 'sql10235766'
